9d90085b35235630d15684d8f2f89a7f3e751c17
Rob <rverplanke@gmail.com>
2025-04-03 13:48:57 +0200

    use two-way data binding

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
+  imports: [FormsModule],
 })
 export class AppNavbar {
   title: string = 'Navbar component';
   isActive: boolean = true;
   isEnabled: boolean = true;
+  userName: string = '';
+  userAdress: string = '';
 
   buttonClick() {
     console.log('clicked');
@@ -19,7 +23,11 @@ export class AppNavbar {
     console.log(event.keyCode);
   }
 
-  keyUpFilter() {
-    console.log('Enter key pressed');
+  keyUpFilter(user: HTMLInputElement) {
+    console.log('Entered value', user.value);
+  }
+
+  updateUserName(username: HTMLInputElement) {
+    this.userName = username.value;
   }
 }
