97f31e27d0c997f4fbe8a50789adb30f3b09758f
Rob <rverplanke@gmail.com>
2025-04-04 21:01:14 +0200

    use ng directives with numbers

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,21 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
 export class FooterComponent {
   title: string = 'Footer component';
+  clickCount: number = 0;
+  isActive: boolean = true;
+
+  // Disable button after 3 clicks
+  handleClick() {
+    this.clickCount++;
+    if (this.clickCount >= 3) this.isActive = false;
+  }
 }
