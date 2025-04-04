0495e00b820372590cc16b1ce73453762c00772e
Rob <rverplanke@gmail.com>
2025-04-04 20:43:25 +0200

    use conditional rendering methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AppNavbar, FooterComponent],
+  imports: [RouterOutlet, AppNavbar, FooterComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'User roles practice';
+  isAdmin: boolean = true;
+  isMember: boolean = false;
+  isGuest: boolean = false;
 }
